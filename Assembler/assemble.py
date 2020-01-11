e5bf4ce4f79d862a3406c37cceeb367e45b98188
Marshall Gates <mggates@gmail.com>
2020-01-11 12:27:22 -0500

    renamed varialbes for clearer meaning

--- Assembler/assemble.py
+++ Assembler/assemble.py
@@ -7,26 +7,29 @@ from Parser import Parser
 def assemble(file_name):
     labels = []
     segments = []
-    m = Memory()
-    i = Instructions()
-    p = Parser()
+    memory = Memory()
+    instructions = Instructions()
+    sap1_parser = Parser()
 
     print("Assemble {}".format(file_name))
-    segments = p.parseFile(file_name)
+    segments = sap1_parser.parseFile(file_name)
 
+    # Extract all the lables from the segments to create a symbol table
     for segment in segments:
         for label in segment.labels:
             labels.append(label)
 
     for segment in segments:
-        segment.assemble(labels, i)
+        segment.assemble(labels, instructions)
 
+    code_segment = None
     for segment in segments:
         if segment.isCode():
             code_segment = segment
-        m = segment.loadMemory(m)
+        memory = segment.loadMemory(memory)
 
-    m.dump(labels, code_segment.cell_list)
+    memory.dump(labels, code_segment)
+
 
 parser = argparse.ArgumentParser(description='Assemble a SAP-1 Assembler file to binary.')
 parser.add_argument('--file', dest="file_name", required=True, help='filename to assemble')
