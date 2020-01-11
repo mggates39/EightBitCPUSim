85bf03073d640b887baf61874c7db242855df2fc
Marshall Gates <mggates@gmail.com>
2020-01-11 12:40:59 -0500

    Update dummy parser to match updated multiply.asm file
    Validate the use of add Label function

--- multiply.asm
+++ multiply.asm
@@ -2,25 +2,27 @@
 # Multipy two numbers x and y
 ###############
 	.corg 0
-Top:		LDA x
-			SUB One
-			JC Continue
-			LDA product
-			OUT
-			HLT
-Continue:	STA x
-			LDA product
-			ADD y
-			STA product
-			JMP Top
+Top:	LDA x
+		SUB One
+		JC Continue
+		LDA product
+		OUT
+		HLT
+Continue:
+		STA x
+		LDA product
+		ADD y
+		STA product
+		JMP Top
 
 	.dorg 12
 # Constant Data
-One:		.byte 1
+One:	.byte 1
 #
 # Variables
-product:	.byte 0
-x:			.byte 3
-y:			.byte 29
+product:
+		.byte 0
+x:		.byte 3
+y:		.byte 29
 
 	.end
