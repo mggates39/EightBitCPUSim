5b1e321a16a59384db6c10cd0f20c0b4cf899017
Marshall Gates <mggates@gmail.com>
2020-02-13 18:30:35 -0500

    Add LEDs to the components

--- IDE/Sap1Emulator/ControlLogic.py
+++ IDE/Sap1Emulator/ControlLogic.py
@@ -1,12 +1,22 @@
 import wx
 from pubsub import pub
 
+from GuiComponents.LedArray import LEDArray
+
+
 class ControlLogic(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, size=(200, 75))
         self.parent = parent
         self.box = wx.StaticBox(self, wx.ID_ANY, "Control Logic", wx.DefaultPosition, (200, 75))
         nmSizer = wx.StaticBoxSizer(self.box, wx.VERTICAL)
+        vertical_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.leds = LEDArray(self.box, 24, topic="cl.set_value")
+
+        vertical_box.Add(self.leds, 1, wx.ALIGN_CENTER | wx.EXPAND, 10)
+
+        nmSizer.Add(vertical_box, 1, wx.EXPAND)
 
         self.SetSizer(nmSizer)
 
--- IDE/Sap1Emulator/InstructionRegister.py
+++ IDE/Sap1Emulator/InstructionRegister.py
@@ -1,12 +1,24 @@
 import wx
 from pubsub import pub
 
+from GuiComponents.LedArray import LEDArray
+
+
 class InstructionRegister(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, size=(100, 75))
         self.parent = parent
         self.box = wx.StaticBox(self, wx.ID_ANY, "Instruction Register", wx.DefaultPosition, (100, 75))
         nmSizer = wx.StaticBoxSizer(self.box, wx.VERTICAL)
+        vertical_box = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.instruction = LEDArray(self.box, 4, topic="ip.set_instruction")
+        self.data = LEDArray(self.box, 4, topic="ip.set_data")
+
+        vertical_box.Add(self.instruction, 1, wx.ALIGN_CENTER | wx.EXPAND, 10)
+        vertical_box.Add(self.data, 1, wx.ALIGN_CENTER | wx.EXPAND, 10)
+
+        nmSizer.Add(vertical_box, 1, wx.EXPAND)
 
         self.SetSizer(nmSizer)
 
