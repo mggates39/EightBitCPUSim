1770b8f57ab61b329105cb710cb4331acebbd73b
Marshall Gates <mggates@gmail.com>
2020-02-22 22:34:08 -0500

    Add the rest of the components

---
+++ IDE/Sap2Emulator/CPU.py
@@ -0,0 +1,82 @@
+import wx
+from pubsub import pub
+
+from Sap1Emulator.OutputRegister import OutputRegister
+from Sap2Emulator.ALU import Alu
+from Sap2Emulator.Accumulator import Accumulator
+from Sap2Emulator.Bus import Bus
+from Sap2Emulator.Clock import Clock
+from Sap2Emulator.ControlLogic import ControlLogic
+from Sap2Emulator.History import ExecutionHistory
+from Sap2Emulator.InstructionRegister import InstructionRegister
+from Sap2Emulator.Memory import Memory
+from Sap2Emulator.MemoryAddressRegister import MemoryAddressRegister
+from Sap2Emulator.MicroCode import MicroCode
+from Sap2Emulator.ProgramCounter import ProgramCounter
+from Sap2Emulator.Reset import Reset
+from Sap2Emulator.StatusRegister import StatusRegister
+from Sap2Emulator.TempRegister import TempRegister
+
+
+class CPU(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size=(1024, 800))
+        self.parent = parent
+        self.box = wx.StaticBox(self, wx.ID_ANY, "SAP 1 CPU", wx.DefaultPosition, (1024, 800))
+        static_box_sizer = wx.StaticBoxSizer(self.box, wx.HORIZONTAL)
+
+        self.microcode_engine = MicroCode()
+        self.reset = Reset(self.box)
+        self.bus = Bus(self.box)
+        self.clock = Clock(self.box)
+        self.pc = ProgramCounter(self.box)
+        self.mar = MemoryAddressRegister(self.box)
+        self.a_register = Accumulator(self.box, name="A")
+        self.b_register = Accumulator(self.box, name="B")
+        self.c_register = Accumulator(self.box, name="C")
+        self.mem = Memory(self.box)
+        self.alu = Alu(self.box)
+        self.tmp = TempRegister(self.box)
+        self.out = OutputRegister(self.box)
+        self.cl = ControlLogic(self.box)
+        self.sr = StatusRegister(self.box)
+        self.ir = InstructionRegister(self.box, self.microcode_engine)
+        self.history = ExecutionHistory(self.box)
+
+        self.sizer = wx.GridBagSizer(10, 10)
+
+        self.sizer.Add(self.reset, pos=(0, 0), flag=wx.EXPAND)
+        self.sizer.Add(self.mar, pos=(1, 0), flag=wx.EXPAND)
+        self.sizer.Add(self.mem, pos=(2, 0), span=(10, 1), flag=wx.EXPAND)
+        self.sizer.Add(self.ir, pos=(12, 0), flag=wx.EXPAND)
+
+        self.sizer.Add(self.clock, pos=(0, 1), span=(1, 2), flag=wx.EXPAND)
+        self.sizer.Add(self.bus, pos=(1, 1), span=(12, 1), flag=wx.EXPAND)
+
+        self.sizer.Add(self.a_register, pos=(1, 2), flag=wx.EXPAND)
+        self.sizer.Add(self.alu, pos=(3, 2), span=(7, 1), flag=wx.EXPAND)
+        self.sizer.Add(self.tmp, pos=(10, 2), flag=wx.EXPAND)
+        self.sizer.Add(self.out, pos=(11, 2), flag=wx.EXPAND)
+        self.sizer.Add(self.cl, pos=(12, 2), span=(1, 2), flag=wx.EXPAND)
+
+        self.sizer.Add(self.pc, pos=(0, 3), flag=wx.EXPAND)
+        self.sizer.Add(self.b_register, pos=(1, 3), flag=wx.EXPAND)
+        self.sizer.Add(self.history, pos=(2, 3), span=(10, 3), flag=wx.EXPAND)
+
+        self.sizer.Add(self.sr, pos=(12, 4), flag=wx.EXPAND)
+        self.sizer.Add(self.c_register, pos=(1, 4), flag=wx.EXPAND)
+
+        static_box_sizer.Add(self.sizer, 1, wx.EXPAND)
+        self.SetAutoLayout(1)
+        self.SetSizer(static_box_sizer)
+        self.sizer.Fit(self)
+
+    def load_memory(self, memory):
+        data = []
+        idx = 0
+        for mem in memory.get_memory_array():
+            data.append(("0x{0:X}:".format(idx), "{0:08b}".format(mem)))
+            idx += 1
+
+        self.mem.load_data(data)
+        pub.sendMessage('CPU.Reset')
---
+++ IDE/Sap2Emulator/MemoryAddressRegister.py
@@ -0,0 +1,59 @@
+import wx
+from pubsub import pub
+
+from GuiComponents.LedArray import LEDArray
+
+
+class MemoryAddressRegister(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size=(250, 100))
+        self.parent = parent
+        self.value = 0
+        self.buffer = 0
+        self.box = wx.StaticBox(self, wx.ID_ANY, "Memory Address Register", wx.DefaultPosition, (250, 100))
+        static_box_sizer = wx.StaticBoxSizer(self.box, wx.VERTICAL)
+        horizontal_box = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.panel = wx.Panel(self.box, size=(30, 75))
+        self.write_indicator = wx.StaticText(self.panel, label="MI")
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(self.write_indicator, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+        self.panel.SetSizer(vbox)
+
+        self.leds = LEDArray(self.box, 16, topic="mar.set_value")
+
+        horizontal_box.Add(self.panel, 0, wx.EXPAND)
+        horizontal_box.Add(self.leds, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.ALL, 10)
+
+        static_box_sizer.Add(horizontal_box, 1, wx.EXPAND)
+
+        self.SetSizer(static_box_sizer)
+
+        pub.subscribe(self.on_clock, 'CPU.Clock')
+        pub.subscribe(self.on_reset, 'CPU.Reset')
+        pub.subscribe(self.on_bus_change, 'CPU.BusChanged')
+        pub.subscribe(self.on_in, 'CPU.MarIn')
+
+    def set_in_display_flag(self):
+        self.write_indicator.SetForegroundColour((0, 0, 255))  # set text color
+
+    def clear_display_flags(self):
+        self.write_indicator.SetForegroundColour((0, 0, 0))  # set text color
+
+    def on_clock(self):
+        self.clear_display_flags()
+
+    def on_reset(self):
+        self.value = 0
+        self.buffer = 0
+        self.on_clock()
+        pub.sendMessage('mar.set_value', new_value=self.value)
+
+    def on_bus_change(self, new_value):
+        self.buffer = new_value
+
+    def on_in(self):
+        self.value = self.buffer
+        self.set_in_display_flag()
+        pub.sendMessage('mar.set_value', new_value=self.value)
+        pub.sendMessage('mem.set_address', new_value=self.value)
