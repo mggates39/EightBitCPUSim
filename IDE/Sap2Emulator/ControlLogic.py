de98aa0dc3643ba305a60f1bc68ec04925b4e583
Marshall Gates <mggates@gmail.com>
2020-02-22 19:42:20 -0500

    Add more components

---
+++ IDE/Sap2Emulator/ControlLogic.py
@@ -0,0 +1,20 @@
+import wx
+
+from GuiComponents.ControlLedArray import ControlLedArray
+
+
+class ControlLogic(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size=(200, 75))
+        self.parent = parent
+        self.box = wx.StaticBox(self, wx.ID_ANY, "Control Logic", wx.DefaultPosition, (200, 75))
+        static_box_sizer = wx.StaticBoxSizer(self.box, wx.VERTICAL)
+        vertical_box = wx.BoxSizer(wx.VERTICAL)
+
+        self.leds = ControlLedArray(self.box, '#0065ef', '#00075f')
+
+        vertical_box.Add(self.leds, 1, wx.ALIGN_CENTER | wx.EXPAND | wx.ALL, 5)
+
+        static_box_sizer.Add(vertical_box, 1, wx.EXPAND)
+
+        self.SetSizer(static_box_sizer)
