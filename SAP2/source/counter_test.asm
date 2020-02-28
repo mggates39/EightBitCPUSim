1906d6d5ebd3dcf96cd5d48034ef5bd9fedbc77e
Marshall Gates <gatesm@lumosnet.com>
2020-02-28 11:31:05 -0500

    Updated Test applications to SAP-2 instruction set

--- SAP2/source/counter_test.asm
+++ SAP2/source/counter_test.asm
@@ -2,17 +2,15 @@
 # Count up and down test OUT display
 ###############
 	.corg 0
-top:	OUT
-	ADD	(data)
+	MVI	A,0
+	MVI	B,1
+top:	OUT	2
+	ADD	B
 	JC	(cont)
 	JMP	(top)
-cont:	SUB	(data)
-	OUT
+cont:	SUB	B
+	OUT	2
 	JZ	(top)
 	JMP	(cont)
-
-	.dorg 15
-# Variables
-data:	.byte 1
 
 	.end
