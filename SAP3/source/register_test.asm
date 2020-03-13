97c3867f10c14c77e5085c3b6b8a07fbcc4e767f
Marshall Gates <mggates@gmail.com>
2020-03-12 23:42:04 -0400

    Add additional instruction testing
    Including pre-loading the stack

--- SAP3/source/register_test.asm
+++ SAP3/source/register_test.asm
@@ -1,14 +1,28 @@
 	.org 0
+	LXI SP,(Stack)
 	MVI A,87
+	INR A
 	MOV B,A
+	INR B
 	MOV C,B
+	INR C
 	MOV D,C
+	INR D
 	MOV E,D
+	INR E
 	MOV H,E
+	INR H
 	MOV L,H
+	INR L
 	PUSH DE
-	LXI DE,(42838)
+	LXI DE,(42738)
 	POP DE
+	MVI H,0
+	MVI L,0
 	HLT
-
+;
+; Top of Stack
+;
+	.org 128
+Stack:	
 	.end
