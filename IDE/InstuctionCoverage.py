d2f6c3ef5f1f26b7fc4cde3ff7559c6be056d805
Marshall Gates <mggates@gmail.com>
2020-03-14 02:27:19 -0400

    Added integration unit tests
    These test ensure that all assembler instructions have microcode and that all microcode has an instruction.

---
+++ IDE/InstuctionCoverage.py
@@ -0,0 +1,41 @@
+import unittest
+from Sap3Assembler.Instructions import Instructions
+from Sap3Emulator.MicroCode import MicroCode
+from Sap3Emulator.MicroCode import operators
+
+class MyTestCase(unittest.TestCase):
+    def test_all_parsable_are_implemented(self):
+
+        instructions = Instructions()
+        microcode = MicroCode()
+        result = True
+
+        for operator in instructions.operators:
+            details = instructions.get_operator(operator)
+            microcode.decode_op_code(details["op_code"])
+            lookup = microcode.get_current_operator()
+            if lookup["operator"] != details["operator"]:
+                print("Warning: {0} - 0x{1:02X} is not implemented.".format(details["operator"], details["op_code"]))
+                result = False
+
+        self.assertEqual(True, result)
+
+    def test_all_implemented_are_parsable(self):
+
+        instructions = Instructions()
+        microcode = MicroCode()
+        result = True
+
+        for op_code in operators:
+            micro_details = operators[op_code]
+            details = instructions.get_operator(micro_details["operator"])
+
+            if details is None:
+                print("Warning: {0} - 0x{1:02X} is not parseable.".format(micro_details["operator"], micro_details["op_code"]))
+                result = False
+
+        self.assertEqual(True, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
