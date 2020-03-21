4ac8741b41d4f123130dd2af7b97cb002fd792ab
Marshall Gates <mggates@gmail.com>
2020-03-21 15:16:32 -0400

    Add test to operators in mnemonics
    Ensure all operators in the instruction set are mapped to operators in the mnemonic list

--- IDE/InstuctionCoverage.py
+++ IDE/InstuctionCoverage.py
@@ -1,7 +1,10 @@
 import unittest
+
 from Sap3Assembler.Instructions import Instructions
 from Sap3Emulator.MicroCode import MicroCode
+from Sap3Emulator.MicroCode import decode_messages
 from Sap3Emulator.MicroCode import operators
+
 
 class MyTestCase(unittest.TestCase):
     def test_all_parsable_are_define(self):
@@ -17,6 +20,32 @@ class MyTestCase(unittest.TestCase):
                     print("Warning: mnemonic {} is not an operator.".format(operator))
                     result = False
 
+        self.assertEqual(True, result)
+
+    def test_all_define_are_parsable(self):
+
+        instructions = Instructions()
+        result = True
+
+        for operator in instructions.operators:
+            list = operator.split(' ')
+            mnemonic = list[0]
+            if instructions.is_mnemonic(mnemonic):
+                details = instructions.get_mnemonic(mnemonic)
+                if operator not in details['operators']:
+                    print("Warning: operator {} not found in mnemonic {} operator list.".format(operator, mnemonic))
+                    result = False
+            else:
+                print("Warning: operator {} does not have mnemonic.".format(operator))
+                result = False
+
+        for mnemonic in instructions.mnemonics:
+            mnemonic_details = instructions.get_mnemonic(mnemonic)
+            for operator in mnemonic_details["operators"]:
+                details = instructions.get_operator(operator)
+                if details is None:
+                    print("Warning: mnemonic {} is not an operator.".format(operator))
+                    result = False
 
         self.assertEqual(True, result)
 
@@ -31,7 +60,8 @@ class MyTestCase(unittest.TestCase):
             microcode.decode_op_code(details["op_code"])
             lookup = microcode.get_current_operator()
             if lookup["operator"] != details["operator"]:
-                print("Warning: operator {0} - 0x{1:02X} is not implemented.".format(details["operator"], details["op_code"]))
+                print("Warning: operator {0} - 0x{1:02X} is not implemented.".format(details["operator"],
+                                                                                     details["op_code"]))
                 result = False
 
         self.assertEqual(True, result)
@@ -39,7 +69,6 @@ class MyTestCase(unittest.TestCase):
     def test_all_implemented_are_parsable(self):
 
         instructions = Instructions()
-        microcode = MicroCode()
         result = True
 
         for op_code in operators:
